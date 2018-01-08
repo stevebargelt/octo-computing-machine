5763d7b56efd4d45d31a189244dd7180b6265be9
Steven.Bargelt <steven.bargelt@target.com>
2018-01-08 09:48:45 -0600

    Working get product but this

--- main.go
+++ main.go
@@ -3,91 +3,101 @@ package main
 import (
 	"fmt"
 
-	"github.com/stevebargelt/missionInterview/apple"
-	"github.com/stevebargelt/missionInterview/binarytree"
-	"github.com/stevebargelt/missionInterview/fibonacci"
-	"github.com/stevebargelt/missionInterview/linkedlist"
-	"github.com/stevebargelt/missionInterview/queue"
-	"github.com/stevebargelt/missionInterview/stack"
+	"github.com/stevebargelt/missionInterview/productbutthis"
 )
 
 func main() {
 
-	fmt.Println("---------- Linked List ----------")
-	// Create a new list and put some numbers in it.
-	l := linkedlist.New()
-	n0 := l.PushFront(1)
-	n2 := l.InsertValue(2, n0)
-	l.InsertValue(3, n2)
-	l.PushFront(0)
+	// fmt.Println("---------- Linked List ----------")
+	// // Create a new list and put some numbers in it.
+	// l := linkedlist.New()
+	// n0 := l.PushFront(1)
+	// n2 := l.InsertValue(2, n0)
+	// l.InsertValue(3, n2)
+	// l.PushFront(0)
 
-	// Iterate through list and print its contents.
-	for n := l.Front(); n != nil; n = n.Next() {
-		fmt.Println(n.Value)
-	}
+	// // Iterate through list and print its contents.
+	// for n := l.Front(); n != nil; n = n.Next() {
+	// 	fmt.Println(n.Value)
+	// }
 
-	fmt.Println("----------")
-	l.Remove(n2)
+	// fmt.Println("----------")
+	// l.Remove(n2)
 
-	for n := l.Front(); n != nil; n = n.Next() {
-		fmt.Println(n.Value)
-	}
+	// for n := l.Front(); n != nil; n = n.Next() {
+	// 	fmt.Println(n.Value)
+	// }
 
-	fmt.Println("---------- Stack ----------")
+	// fmt.Println("---------- Stack ----------")
 
-	stk := stack.NewStack()
-	stk.Push(0)
-	stk.Push(1)
-	stk.Push(2)
+	// stk := stack.NewStack()
+	// stk.Push(0)
+	// stk.Push(1)
+	// stk.Push(2)
 
-	fmt.Printf("POP 1 = %v\n", stk.Pop())
-	fmt.Printf("POP 2 = %v\n", stk.Pop())
-	fmt.Printf("POP 3 = %v\n", stk.Pop())
+	// fmt.Printf("POP 1 = %v\n", stk.Pop())
+	// fmt.Printf("POP 2 = %v\n", stk.Pop())
+	// fmt.Printf("POP 3 = %v\n", stk.Pop())
 
-	fmt.Println("---------- Queue ----------")
+	// fmt.Println("---------- Queue ----------")
 
-	q := queue.NewQueue()
-	q.Enqueue(0)
-	q.Enqueue(1)
-	q.Enqueue(2)
+	// q := queue.NewQueue()
+	// q.Enqueue(0)
+	// q.Enqueue(1)
+	// q.Enqueue(2)
 
-	fmt.Printf("Dequeue 1 = %v\n", q.Dequeue())
-	fmt.Printf("Dequeue 2 = %v\n", q.Dequeue())
-	fmt.Printf("Dequeue 3 = %v\n", q.Dequeue())
+	// fmt.Printf("Dequeue 1 = %v\n", q.Dequeue())
+	// fmt.Printf("Dequeue 2 = %v\n", q.Dequeue())
+	// fmt.Printf("Dequeue 3 = %v\n", q.Dequeue())
 
-	// fmt.Println("---------- Queue2 ----------")
+	// // fmt.Println("---------- Queue2 ----------")
 
-	// q2 := queue2.NewQueue2()
-	// q2.Enqueue(0)
-	// q2.Enqueue(1)
-	// q2.Enqueue(2)
+	// // q2 := queue2.NewQueue2()
+	// // q2.Enqueue(0)
+	// // q2.Enqueue(1)
+	// // q2.Enqueue(2)
 
-	// fmt.Printf("Dequeue 1 = %d\n", q2.Dequeue())
-	// fmt.Printf("Dequeue 2 = %d\n", q2.Dequeue())
-	// fmt.Printf("Dequeue 3 = %d\n", q2.Dequeue())
+	// // fmt.Printf("Dequeue 1 = %d\n", q2.Dequeue())
+	// // fmt.Printf("Dequeue 2 = %d\n", q2.Dequeue())
+	// // fmt.Printf("Dequeue 3 = %d\n", q2.Dequeue())
 
-	fmt.Println("---------- Binary Tree ----------")
+	// fmt.Println("---------- Binary Tree ----------")
 
-	root := binarytree.New(1)
-	r1 := root.AddRight(2)
-	l1 := root.AddLeft(3)
+	// root := binarytree.New(1)
+	// r1 := root.AddRight(2)
+	// l1 := root.AddLeft(3)
 
-	r1.AddRight(4).AddRight(5)
-	l1.AddLeft(7)
-	fmt.Print(root)
+	// r1.AddRight(4).AddRight(5)
+	// l1.AddLeft(7)
+	// fmt.Print(root)
 
-	fmt.Printf("\n\n---------- Fibonacci ----------\n")
+	// fmt.Printf("\n\n---------- Fibonacci ----------\n")
 
-	fmt.Printf("Sequence: %v\n", fibonacci.Fibs(10))
-	fmt.Printf("Nth Fib: %v\n", fibonacci.Fib(10))
-	fmt.Printf("Nth Fib: %v\n", fibonacci.FibSmaller(10))
-	fmt.Printf("Nth Fib: %v\n", fibonacci.FibRecurse(10))
-	fmt.Printf("Nth Fib: %v\n", fibonacci.FibTail(10))
+	// fmt.Printf("Sequence: %v\n", fibonacci.Fibs(10))
+	// fmt.Printf("Nth Fib: %v\n", fibonacci.Fib(10))
+	// fmt.Printf("Nth Fib: %v\n", fibonacci.FibSmaller(10))
+	// fmt.Printf("Nth Fib: %v\n", fibonacci.FibRecurse(10))
+	// fmt.Printf("Nth Fib: %v\n", fibonacci.FibTail(10))
 
-	prices := []int{10, 7, 5, 8, 11, 4, 9}
-	maxProfit, err := apple.GetMaxProfit(prices)
+	// prices := []int{10, 7, 5, 8, 11, 4, 9}
+	// maxProfit, err := apple.GetMaxProfit(prices)
+	// if err != nil {
+	// 	fmt.Printf("Error: %v", err)
+	// }
+	// fmt.Printf("GetMaxProfit: %v\n", maxProfit)
+
+	nums := []int{1, 7, 3, 4}
+	result, err := productbutthis.GetProductsOfAllIntsExceptAtIndex(nums)
 	if err != nil {
 		fmt.Printf("Error: %v", err)
 	}
-	fmt.Printf("GetMaxProfit: %v\n", maxProfit)
+	fmt.Printf("Product but this %v\n", result)
+
+	nums = []int{0, 7, 3, 4}
+	result, err = productbutthis.GetProductsOfAllIntsExceptAtIndex(nums)
+	if err != nil {
+		fmt.Printf("Error: %v", err)
+	}
+	fmt.Printf("Zeros Product but this %v\n", result)
+
 }
--- productbutthis/productbutthis.go
+++ productbutthis/productbutthis.go
@@ -1,6 +1,36 @@
 package productbutthis
 
-func getProductsOfAllIntsExceptAtIndex(arry []int) (int, error) {
+import "errors"
 
-	return 0, nil
+// GetProductsOfAllIntsExceptAtIndex returns the product of all the integers in
+//		in an array except the current member of the array.
+//		Example: in a 4 element array:
+//					position 0 would be [1]*[2]*[3]
+//					position 1 would be [0]*[2]*[3]
+//					position 2 would be [0]*[1]*[3]
+//					position 3 would be [0]*[1]*[2]
+// O(n) - time
+// O(n) - space
+func GetProductsOfAllIntsExceptAtIndex(arry []int) ([]int, error) {
+
+	if arry == nil {
+		return nil, errors.New("Input array cannot be nil")
+	}
+	if len(arry) < 2 {
+		return nil, errors.New("Need at least two values to multiply")
+	}
+
+	result := make([]int, len(arry))
+	rolling := 1
+	for i := 0; i < len(arry); i++ {
+		result[i] = rolling
+		rolling *= arry[i]
+	}
+
+	rolling = 1
+	for i := len(arry) - 1; i >= 0; i-- {
+		result[i] *= rolling
+		rolling *= arry[i]
+	}
+	return result, nil
 }
